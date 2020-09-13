548f606db0df28d3e1945a0443ecde627420a8f6
Ruslan Vasylenko <jokertube602@gmail.com>
2020-09-13 19:17:00 +0300

    mod5_sol

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -40,6 +40,9 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
+  if (propName === "randomCategoryShortName"){
+    propToReplace = propName;
+  }
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
   return string;
@@ -102,7 +105,6 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories);
-      console.log(chosenCategoryShortName)
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
