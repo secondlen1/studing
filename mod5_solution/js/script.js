8cff489bda25be2875619c92fa0099b06f43a23f
Ruslan Vasylenko <jokertube602@gmail.com>
2020-09-13 19:36:56 +0300

    mod5_sol

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -40,9 +40,6 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
-  if (propName === "randomCategoryShortName"){
-    propToReplace =  propName;
-  }
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
   return string;
@@ -104,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -117,7 +114,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', chooseRandomCategory);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"'" + 'randomCategoryShortName' + "'", chooseRandomCategory);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
